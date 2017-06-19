8514acd16a2befdae12b01d430185023ba5c2950
cosmegt <caneko@live.com>
2017-06-19 00:50:58 -0700

    perfectioning sidebar

--- src/js/main.js
+++ src/js/main.js
@@ -3,5 +3,11 @@ $('.trigger').click(function(){
     $('.sidebar').animate({
         right: '0px'
     },200);
-    $('')
+    $('blocker').css('display','block');
+    $('blocker').click(function(){
+        $('.sidebar').animate({
+            right: '-257px'
+        },200)
+        $('blocker').css('display', 'none')
+    })
 })
