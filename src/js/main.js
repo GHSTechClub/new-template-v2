4e7f73d232fe96b7627bd62463f8af0ed21c6964
cosmegt <caneko@live.com>
2017-06-19 01:22:37 -0700

    -Fixing minor bugs in sidebar -Added Images -Fixing Animations -Animating Links on Hover Action

--- src/js/main.js
+++ src/js/main.js
@@ -2,12 +2,12 @@ $(document).ready(function(){/*lol*/})
 $('.trigger').click(function(){
     $('.sidebar').animate({
         right: '0px'
-    },200);
+    },300);
     $('blocker').css('display','block');
     $('blocker').click(function(){
         $('.sidebar').animate({
             right: '-257px'
-        },200)
+        },300)
         $('blocker').css('display', 'none')
     })
 })
